e0226d48cfdfe50c7c24746c8c938c10f1b33b30
Alejandro Garcia <painisprep@gmail.com>
2025-10-15 17:02:20 -0400

    feat(AutoPilot): base logic completed

--- src/models/AutoPilot/AutoPilot.js
+++ src/models/AutoPilot/AutoPilot.js
@@ -1,12 +1,41 @@
 export default class AutoPilot {
-  constructor(oponentGameboard) {
-    this.targetBoard = oponentGameboard.board;
+  constructor(player, oponentGameboard) {
+    this.player = player;
+    this.targetGameboard = oponentGameboard;
+    this.targetBoard = this.targetGameboard.board;
+    this.totalShips = this.targetGameboard.ships.length;
+    this.shipLengths = [5, 4, 3, 3, 2];
+    this.targetShips = new Array(this.totalShips)
+      .fill(null)
+      .map((ship, index) => ({
+        index,
+        positions: [],
+        length: this.shipLengths[index % this.totalShips],
+        isSunk: false,
+      }));
+    this.directions = [
+      { dir: 'up', val: -10 },
+      { dir: 'right', val: 1 },
+      { dir: 'down', val: 10 },
+      { dir: 'left', val: -1 },
+    ];
   }
 
-  getAttackPositions(numberOfAttacks) {}
+  getAttackPositions() {
+    const hitCells = this.findHitCells();
+    let attackPositions = [];
+
+    if (!hitCells) {
+      return this.getRandomAttackPositions();
+    }
+
+    attackPositions = this.explorePositions(hitCells);
+    return attackPositions;
+  }
 
   findHitCells() {
     const cellsIndex = [];
+    const sunkShipsIndex = [];
 
     for (let i = 0; i < this.targetBoard.length; i += 1) {
       const cell = this.targetBoard[i];
@@ -14,6 +43,13 @@ export default class AutoPilot {
       if (cell.status === 2) {
         cellsIndex.push(cell.index);
       }
+
+      if (cell.status === 3) {
+        sunkShipsIndex.push(cell.index);
+      }
+    }
+    if (sunkShipsIndex.length !== 0) {
+      this.registerSunkShips(sunkShipsIndex);
     }
 
     if (cellsIndex.length === 0) {
@@ -21,5 +57,314 @@ export default class AutoPilot {
     }
 
     return cellsIndex;
+  }
+
+  registerSunkShips(positions) {
+    const remainingPositions = positions;
+
+    this.targetShips.forEach((ship) => {
+      const foundPositions = this.findPositionsForShipLength(
+        ship.length,
+        remainingPositions
+      );
+
+      foundPositions.forEach((p) => {
+        const i = remainingPositions.findIndex((el) => p === el);
+        remainingPositions.splice(i, 1);
+      });
+
+      if (foundPositions.length > 0) {
+        ship.positions = foundPositions;
+        ship.isSunk = true;
+      }
+    });
+  }
+
+  findPositionsForShipLength(length, positions) {
+    let shipPos = [];
+
+    for (let pos = 0; pos < positions.length; pos += 1) {
+      const head = positions[pos];
+      shipPos = [head];
+
+      for (let dir = 0; dir < this.directions.length; dir += 1) {
+        for (let i = 1; i <= length; i += 1) {
+          const next = head + i * this.directions[dir].val;
+          if (positions.includes(next)) shipPos.push(next);
+          if (shipPos.length === length) return shipPos;
+        }
+      }
+    }
+    return [];
+  }
+
+  getPositionsLeft(ship) {
+    return ship.length - ship.positions.length;
+  }
+
+  explorePositions(hitCells) {
+    this.assignHitsToShips(hitCells);
+
+    if (hitCells.length === 1) {
+      return this.getAttacksAroundPosition(
+        hitCells[0],
+        this.player.getRoundTargetsLeft()
+      );
+    }
+
+    const shipsAlive = [];
+    const positions = [];
+
+    this.targetShips.forEach((ship) => {
+      if (!ship.isSunk) shipsAlive.push(ship);
+    });
+
+    let numberOfAttacks = this.player.getRoundTargetsLeft();
+
+    while (numberOfAttacks > 0) {
+      const shipCandidate = this.findShipCandidate(shipsAlive);
+
+      if (!shipCandidate) {
+        for (let i = 0; i < numberOfAttacks; i += 1) {
+          positions.push(this.getRandomAttackPosition());
+        }
+      }
+
+      if (shipCandidate) {
+        const index = shipsAlive.findIndex(
+          (ship) => ship.index === shipCandidate.index
+        );
+        shipsAlive.splice(index, 1);
+
+        numberOfAttacks =
+          numberOfAttacks >= this.getPositionsLeft(shipCandidate)
+            ? this.getPositionsLeft(shipCandidate)
+            : numberOfAttacks;
+
+        positions.push(
+          ...this.getAttackPositionsForCandidate(shipCandidate, numberOfAttacks)
+        );
+      }
+
+      numberOfAttacks = this.player.getRoundTargetsLeft() - positions.length;
+    }
+
+    return positions;
+  }
+
+  findShipCandidate(shipsAlive) {
+    let shipCandidate = shipsAlive[0];
+
+    if (shipsAlive.length > 1) {
+      for (let i = 1; i < shipsAlive.length; i += 1) {
+        const comparingShip = shipsAlive[i];
+        if (
+          this.getPositionsLeft(comparingShip) <
+            this.getPositionsLeft(shipCandidate) &&
+          comparingShip.positions.length > 0
+        )
+          shipCandidate = comparingShip;
+      }
+    }
+
+    if (shipCandidate.positions.length === 0) return undefined;
+
+    return shipCandidate;
+  }
+
+  getAttackPositionsForCandidate(shipCandidate, numberOfAttacks) {
+    const positions = [];
+    if (shipCandidate.positions.length > 1) {
+      const rowsCols = this.targetGameboard.rows;
+
+      const sameRow = shipCandidate.positions.every(
+        (position) =>
+          Math.floor(position / rowsCols) ===
+          Math.floor(shipCandidate.positions[0] / rowsCols)
+      );
+
+      const sameColumn = shipCandidate.positions.every(
+        (position) =>
+          position % rowsCols === shipCandidate.positions[0] % rowsCols
+      );
+
+      let dirs;
+      let isRow = false;
+
+      if (sameRow) {
+        dirs = [-1, 1];
+        isRow = true;
+      }
+      if (sameColumn) {
+        dirs = [-10, 10];
+      }
+      const head = shipCandidate.positions[0];
+      const tail = shipCandidate.positions[shipCandidate.positions.length - 1];
+      const dirOne = this.targetGameboard.isValidAttack(head + dirs[0]);
+      const dirTwo = this.targetGameboard.isValidAttack(tail + dirs[1]);
+      const dirOneValue = dirs[0];
+      const dirTwoValue = dirs[1];
+
+      if (dirOne && !dirTwo) {
+        const attacks = this.getAttacksFromDirection(
+          numberOfAttacks,
+          head,
+          dirOneValue,
+          isRow
+        );
+        positions.push(...attacks);
+      }
+
+      if (!dirOne && dirTwo) {
+        const attacks = this.getAttacksFromDirection(
+          numberOfAttacks,
+          tail,
+          dirTwoValue,
+          isRow
+        );
+        positions.push(...attacks);
+      }
+
+      if (dirOne && dirTwo) {
+        const numberOfAttacksDirOne = Math.floor(numberOfAttacks / 2);
+        const numberOfAttacksDirTwo = numberOfAttacks - numberOfAttacksDirOne;
+
+        const attacksDirOne = this.getAttacksFromDirection(
+          numberOfAttacksDirOne,
+          head,
+          dirOneValue,
+          isRow
+        );
+        const attacksDirTwo = this.getAttacksFromDirection(
+          numberOfAttacksDirTwo,
+          tail,
+          dirTwoValue,
+          isRow
+        );
+
+        positions.push(...attacksDirOne);
+        positions.push(...attacksDirTwo);
+      }
+    }
+
+    if (shipCandidate.positions.length === 1) {
+      positions.push(
+        ...this.getAttacksAroundPosition(
+          shipCandidate.positions[0],
+          numberOfAttacks
+        )
+      );
+    }
+
+    return positions;
+  }
+
+  getAttacksFromDirection(numberOfAttacks, start, dir, isRow) {
+    let head = start;
+    let positions = [];
+    let min;
+    let max;
+    const { rows, columns } = this.targetGameboard;
+    const row = Math.floor(start / rows);
+    const col = start - row * columns;
+
+    if (!isRow) {
+      min = 0 + col;
+      max = (rows - 1) * columns + col;
+    }
+
+    if (isRow) {
+      min = row * columns;
+      max = min + columns - 1;
+    }
+
+    for (let i = 0; i < numberOfAttacks; i += 1) {
+      const next = head + dir;
+      if (
+        !this.targetGameboard.isValidAttack(next) ||
+        next < min ||
+        next > max ||
+        this.player.attacksQueue.includes(next)
+      )
+        break;
+      head = next;
+      positions.push(next);
+    }
+
+    return positions;
+  }
+
+  assignHitsToShips(hitCells) {
+    const shipsAlive = [];
+    const unassignedHits = [...hitCells];
+    this.targetShips.forEach((ship) => {
+      if (!ship.isSunk) shipsAlive.push(ship);
+    });
+
+    shipsAlive.forEach(({ length, index }) => {
+      let positions = [];
+      let shipLength = length;
+      while (positions.length === 0 && unassignedHits.length !== 0) {
+        positions = this.findPositionsForShipLength(shipLength, unassignedHits);
+        shipLength -= 1;
+      }
+      if (positions.length === length) positions = [];
+
+      this.targetShips[index].positions = positions;
+
+      positions.forEach((p) => {
+        const i = unassignedHits.findIndex((el) => el === p);
+        unassignedHits.splice(i, 1);
+      });
+    });
+  }
+
+  getAttacksAroundPosition(position, numberOfAttacks) {
+    const positions = [];
+    const maxAttacks = numberOfAttacks;
+    const head = position;
+    let loopCounter = 1;
+    let dirCounter = 0;
+
+    while (positions.length < maxAttacks) {
+      if (dirCounter > 3) {
+        dirCounter = 0;
+        loopCounter += 1;
+      }
+
+      const next = head + this.directions[dirCounter].val * loopCounter;
+
+      if (
+        this.targetGameboard.isValidAttack(next) &&
+        !this.player.attacksQueue.includes(next)
+      )
+        positions.push(next);
+
+      dirCounter += 1;
+    }
+
+    return positions;
+  }
+
+  getRandomAttackPositions() {
+    const attackPositions = [];
+    for (let i = 0; i < this.player.getRoundTargetsLeft(); i += 1) {
+      attackPositions.push(this.getRandomAttackPosition());
+    }
+
+    return attackPositions;
+  }
+
+  getRandomAttackPosition() {
+    let position = -1;
+
+    while (
+      !this.targetGameboard.isValidAttack(position) &&
+      !this.player.attacksQueue.includes(position)
+    ) {
+      position = Math.floor(Math.random() * this.targetGameboard.size);
+    }
+
+    return position;
   }
 }
--- src/models/AutoPilot/AutoPilot.test.js
+++ src/models/AutoPilot/AutoPilot.test.js
@@ -1,33 +1,400 @@
-import Gameboard from '../Gameboard/Gameboard';
-import AutoPilot from './AutoPilot';
+import Gameboard from '../Gameboard/Gameboard.js';
+import AutoPilot from './AutoPilot.js';
+import Player from '../Player/Player.js';
 
 describe('AutoPilot', () => {
-  const testGameboard = new Gameboard(10, 5);
-  const attackedPositions = [0, 29, 42, 65, 99];
-  const shipPositions = [
-    [0, 1, 2, 3, 4],
-    [9, 19, 29, 39],
-    [40, 41, 42],
-    [65, 66, 67],
-    [89, 99],
+  const testsNoHits = [
+    {
+      sunkShips: [],
+      hitPositions: [],
+      attacksQueue: [],
+      expectedNumberOfAttacks: 5,
+    },
+    {
+      sunkShips: [0],
+      hitPositions: [],
+      attacksQueue: [],
+      expectedNumberOfAttacks: 4,
+    },
+    {
+      sunkShips: [0, 1, 2, 3],
+      hitPositions: [],
+      attacksQueue: [],
+      expectedNumberOfAttacks: 1,
+    },
+    {
+      sunkShips: [0, 1, 2, 3, 4],
+      hitPositions: [],
+      attacksQueue: [],
+      expectedNumberOfAttacks: 0,
+    },
+    {
+      sunkShips: [],
+      hitPositions: [],
+      attacksQueue: [20, 21],
+      expectedNumberOfAttacks: 3,
+    },
+    {
+      sunkShips: [],
+      hitPositions: [24, 25, 26, 27, 28],
+      attacksQueue: [20, 21],
+      expectedNumberOfAttacks: 3,
+    },
+    {
+      sunkShips: [],
+      hitPositions: [35],
+      attacksQueue: [20, 21],
+      expectedNumberOfAttacks: 3,
+    },
   ];
 
-  shipPositions.forEach((positions, shipIndex) =>
-    testGameboard.placeShip(shipIndex, positions)
-  );
+  const testsOneHit = [
+    {
+      sunkShips: [],
+      hitPositions: [42],
+      attacksQueue: [],
+      expectedAttacks: [32, 43, 52, 41, 22],
+      expectedNumberOfAttacks: 5,
+    },
+    {
+      sunkShips: [0, 1, 2, 3],
+      hitPositions: [42],
+      attacksQueue: [],
+      expectedAttacks: [32],
+      expectedNumberOfAttacks: 1,
+    },
+    {
+      sunkShips: [1],
+      hitPositions: [42],
+      attacksQueue: [],
+      expectedAttacks: [32, 43, 52, 41],
+      expectedNumberOfAttacks: 4,
+    },
+    {
+      sunkShips: [],
+      hitPositions: [99],
+      attacksQueue: [],
+      expectedAttacks: [89, 98, 79, 97, 69],
+      expectedNumberOfAttacks: 5,
+    },
+    {
+      sunkShips: [],
+      hitPositions: [99],
+      attacksQueue: [89, 79],
+      expectedAttacks: [98, 97, 69],
+      expectedNumberOfAttacks: 5,
+    },
+  ];
 
-  attackedPositions.forEach((positon) => testGameboard.receiveAttack(positon));
+  const testsMoreHits = [
+    {
+      sunkShips: [],
+      hitPositions: [0, 1],
+      attacksQueue: [],
+      expectedAttacks: [2, 3, 4],
+      expectedNumberOfAttacks: 5,
+    },
+    {
+      sunkShips: [],
+      hitPositions: [40, 41, 65, 66],
+      attacksQueue: [],
+      expectedAttacks: [42],
+      expectedNumberOfAttacks: 5,
+    },
+    {
+      sunkShips: [],
+      hitPositions: [19, 29],
+      attacksQueue: [],
+      expectedAttacks: [9, 39],
+      expectedNumberOfAttacks: 5,
+    },
+    {
+      sunkShips: [],
+      hitPositions: [89],
+      attacksQueue: [],
+      expectedAttacks: [99],
+      expectedNumberOfAttacks: 5,
+    },
+    {
+      sunkShips: [0, 1, 2],
+      hitPositions: [40, 41],
+      attacksQueue: [],
+      expectedAttacks: [42],
+      expectedNumberOfAttacks: 2,
+    },
+    {
+      sunkShips: [0, 2, 3],
+      hitPositions: [9, 19],
+      attacksQueue: [],
+      expectedAttacks: [29, 39],
+      expectedNumberOfAttacks: 2,
+    },
+    {
+      sunkShips: [0, 2, 3],
+      hitPositions: [9, 19],
+      attacksQueue: [29],
+      expectedAttacks: [],
+      expectedNumberOfAttacks: 1,
+    },
+  ];
 
-  const autoPilot = new AutoPilot(testGameboard);
+  describe('getAttackPositions', () => {
+    let player;
+    let oponentGameboard;
+    let autoPilot;
 
-  describe('findHitCells', () => {
-    it('should return false if there are no cells hit', () => {
-      const tempAutoPilot = new AutoPilot(new Gameboard());
-      expect(tempAutoPilot.findHitCells()).toBe(false);
+    beforeEach(() => {
+      player = new Player('A', 10, 5, true);
+      oponentGameboard = new Gameboard(10, 5);
+      const shipPositions = [
+        [0, 1, 2, 3, 4],
+        [9, 19, 29, 39],
+        [40, 41, 42],
+        [65, 66, 67],
+        [89, 99],
+      ];
+      shipPositions.forEach((positions, shipIndex) => {
+        player.placeShip(shipIndex, positions);
+        oponentGameboard.placeShip(shipIndex, positions);
+      });
     });
 
-    it('should find all the cells that have been hit', () => {
-      expect(autoPilot.findHitCells()).toEqual(attackedPositions);
+    describe('with no hits (status 2) on the board', () => {
+      testsNoHits.forEach(
+        (
+          { sunkShips, hitPositions, attacksQueue, expectedNumberOfAttacks },
+          index
+        ) => {
+          it(`#${index}: should return ${expectedNumberOfAttacks} attacks when ${sunkShips.length} ships sunk, board has ${hitPositions.length} hit positions, has ${attacksQueue.length} attacks on queue`, () => {
+            if (sunkShips.length > 0) {
+              sunkShips.forEach((shipIndex) => {
+                player.sinkShip(shipIndex);
+              });
+            }
+
+            if (hitPositions.length > 0)
+              hitPositions.forEach((position) =>
+                oponentGameboard.receiveAttack(position)
+              );
+
+            if (attacksQueue.length > 0)
+              attacksQueue.forEach((attack) => player.enqueueAttack(attack));
+
+            autoPilot = new AutoPilot(player, oponentGameboard);
+
+            expect(autoPilot.getAttackPositions().length).toBe(
+              expectedNumberOfAttacks
+            );
+          });
+        }
+      );
+
+      describe('Only 1 hit registered', () => {
+        testsOneHit.forEach(
+          (
+            {
+              sunkShips,
+              hitPositions,
+              attacksQueue,
+              expectedAttacks,
+              expectedNumberOfAttacks,
+            },
+            index
+          ) => {
+            it(`#${index}: should return ${expectedNumberOfAttacks} attacks (${expectedAttacks}) when ${sunkShips.length} ships sunk, board has ${hitPositions.length} hit positions, has ${attacksQueue.length} attacks on queue`, () => {
+              if (sunkShips.length > 0) {
+                sunkShips.forEach((shipIndex) => {
+                  player.sinkShip(shipIndex);
+                });
+              }
+
+              if (hitPositions.length > 0)
+                hitPositions.forEach((position) =>
+                  oponentGameboard.receiveAttack(position)
+                );
+
+              if (attacksQueue.length > 0)
+                attacksQueue.forEach((attack) => player.enqueueAttack(attack));
+
+              autoPilot = new AutoPilot(player, oponentGameboard);
+
+              expect(autoPilot.getAttackPositions()).toEqual(expectedAttacks);
+            });
+          }
+        );
+      });
+
+      describe('multiple hits registered', () => {
+        testsMoreHits.forEach(
+          (
+            {
+              sunkShips,
+              hitPositions,
+              attacksQueue,
+              expectedAttacks,
+              expectedNumberOfAttacks,
+            },
+            index
+          ) => {
+            it(`#${index}: should return ${expectedNumberOfAttacks} attacks (${expectedAttacks}) when ${sunkShips.length} ships sunk, board has ${hitPositions.length} hit positions, has ${attacksQueue.length} attacks on queue`, () => {
+              if (sunkShips.length > 0) {
+                sunkShips.forEach((shipIndex) => {
+                  player.sinkShip(shipIndex);
+                });
+              }
+
+              if (hitPositions.length > 0)
+                hitPositions.forEach((position) =>
+                  oponentGameboard.receiveAttack(position)
+                );
+
+              if (attacksQueue.length > 0)
+                attacksQueue.forEach((attack) => player.enqueueAttack(attack));
+
+              autoPilot = new AutoPilot(player, oponentGameboard);
+
+              const attackPositions = autoPilot.getAttackPositions();
+
+              expect(
+                expectedAttacks.every((p) => attackPositions.includes(p))
+              ).toBe(true);
+              expect(attackPositions.length).toBe(expectedNumberOfAttacks);
+            });
+          }
+        );
+      });
+    });
+  });
+
+  describe('registerSunkShips', () => {
+    let player;
+    let oponentGameboard;
+    let autoPilot;
+
+    beforeEach(() => {
+      player = new Player('A', 10, 5, true);
+      oponentGameboard = new Gameboard(10, 5);
+      const shipPositions = [
+        [0, 1, 2, 3, 4],
+        [9, 19, 29, 39],
+        [40, 41, 42],
+        [65, 66, 67],
+        [89, 99],
+      ];
+      shipPositions.forEach((positions, shipIndex) => {
+        player.placeShip(shipIndex, positions);
+        oponentGameboard.placeShip(shipIndex, positions);
+      });
+    });
+
+    it('should register 5 ship positions when 5 ships are sunk', () => {
+      oponentGameboard.sinkAllShips();
+      autoPilot = new AutoPilot(player, oponentGameboard);
+      autoPilot.getAttackPositions();
+      const pos = [];
+      autoPilot.targetShips.forEach((ship) => pos.push(ship.positions));
+
+      expect(pos).toEqual([
+        [0, 1, 2, 3, 4],
+        [9, 19, 29, 39],
+        [40, 41, 42],
+        [65, 66, 67],
+        [89, 99],
+      ]);
+      expect(autoPilot.targetShips.every((ship) => ship.isSunk)).toBe(true);
+    });
+
+    it('should register 1 ship positions when 1 ship is sunk', () => {
+      oponentGameboard.sinkShip(0);
+      autoPilot = new AutoPilot(player, oponentGameboard);
+      autoPilot.getAttackPositions();
+      const pos = [];
+      autoPilot.targetShips.forEach((ship) => pos.push(ship.positions));
+
+      expect(pos).toEqual([[0, 1, 2, 3, 4], [], [], [], []]);
+      expect(autoPilot.targetShips.every((ship) => ship.isSunk)).toBe(false);
+    });
+  });
+
+  describe('registerHitsToShips', () => {
+    let player;
+    let oponentGameboard;
+    let autoPilot;
+
+    beforeEach(() => {
+      player = new Player('A', 10, 5, true);
+      oponentGameboard = new Gameboard(10, 5);
+      const shipPositions = [
+        [0, 1, 2, 3, 4],
+        [9, 19, 29, 39],
+        [40, 41, 42],
+        [65, 66, 67],
+        [89, 99],
+      ];
+      shipPositions.forEach((positions, shipIndex) => {
+        player.placeShip(shipIndex, positions);
+        oponentGameboard.placeShip(shipIndex, positions);
+      });
+    });
+
+    it('should assign 1 hit to each ship', () => {
+      oponentGameboard.receiveAttack(0);
+      oponentGameboard.receiveAttack(9);
+      oponentGameboard.receiveAttack(40);
+      oponentGameboard.receiveAttack(65);
+      oponentGameboard.receiveAttack(89);
+
+      autoPilot = new AutoPilot(player, oponentGameboard);
+      autoPilot.getAttackPositions();
+      const pos = [];
+      autoPilot.targetShips.forEach((ship) => pos.push(ship.positions));
+
+      expect(pos).toEqual([[0], [9], [40], [65], [89]]);
+    });
+
+    it('should assign 2 hits to 1 ship', () => {
+      oponentGameboard.receiveAttack(0);
+      oponentGameboard.receiveAttack(1);
+
+      autoPilot = new AutoPilot(player, oponentGameboard);
+      autoPilot.getAttackPositions();
+      const pos = [];
+      autoPilot.targetShips.forEach((ship) => pos.push(ship.positions));
+
+      expect(pos).toEqual([[0, 1], [], [], [], []]);
+    });
+
+    it('should assign 3 hits to ship of length 4', () => {
+      oponentGameboard.sinkShip(0);
+      oponentGameboard.receiveAttack(9);
+      oponentGameboard.receiveAttack(19);
+      oponentGameboard.receiveAttack(29);
+
+      autoPilot = new AutoPilot(player, oponentGameboard);
+      autoPilot.getAttackPositions();
+      const pos = [];
+      autoPilot.targetShips.forEach((ship) => pos.push(ship.positions));
+      expect(pos).toEqual([[0, 1, 2, 3, 4], [9, 19, 29], [], [], []]);
+    });
+
+    it('should assign 1 hit to ship of length 2', () => {
+      oponentGameboard.sinkShip(0);
+      oponentGameboard.sinkShip(1);
+      oponentGameboard.sinkShip(2);
+      oponentGameboard.sinkShip(3);
+      oponentGameboard.receiveAttack(99);
+
+      autoPilot = new AutoPilot(player, oponentGameboard);
+      autoPilot.getAttackPositions();
+      const pos = [];
+      autoPilot.targetShips.forEach((ship) => pos.push(ship.positions));
+      expect(pos).toEqual([
+        [0, 1, 2, 3, 4],
+        [9, 19, 29, 39],
+        [40, 41, 42],
+        [65, 66, 67],
+        [99],
+      ]);
     });
   });
 });
